9861bc5517d9066ca41f98e39fd397e1e0391449
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-01 19:06:17 -0300

    adiciona as actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_CURRENCIES = 'SAVE_CURRENCIES';
+
+export const saveEmail = (email) => ({
+  type: SAVE_EMAIL,
+  payload: email,
+});
+
+export const saveCurrencies = (currencies) => ({
+  type: SAVE_CURRENCIES,
+  payload: currencies,
+});
