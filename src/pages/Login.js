cae5ff98806dc39195dcb37f62bc77a4d01ddf7e
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-01 19:36:58 -0300

    remove import do css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { saveEmail } from '../actions';
-import './Login.css';
 
 class Login extends React.Component {
   state = {
