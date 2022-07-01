ba5fdbe8b8a12d0dd67323340f33c46ba06b3bff
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-01 19:16:48 -0300

    adiciona componente wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import fetchApi from '../services';
+import { saveCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount = async () => {
+    const { dispatch } = this.props;
+    const fetch = await fetchApi();
+    dispatch(
+      saveCurrencies(
+        Object.keys(fetch).filter((currencie) => currencie !== 'USDT'),
+      ),
+    );
+  };
+
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <h1>TrybeWallet</h1>
+        <Header />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Wallet.propTypes = {
+  dispatch: PropTypes.func,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
