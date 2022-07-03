991a63e617a1fcdc0c68e5b0a43db3c12168aff0
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-03 16:25:26 -0300

    adiciona mais um case no reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { SAVE_CURRENCIES } from '../actions';
+import { SAVE_CURRENCIES, SAVE_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -13,6 +13,11 @@ const wallet = (state = INITIAL_STATE, action) => {
     return {
       ...state,
       currencies: action.payload,
+    };
+  case SAVE_EXPENSE:
+    return {
+      ...state,
+      expenses: [...state.expenses, { ...action.payload }],
     };
   default:
     return state;
