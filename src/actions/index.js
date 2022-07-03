4ab1ec75344963220a2a24ca7a5f7570bbdbf2fc
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-03 16:24:13 -0300

    cria thunk creators para salvar o retorno assíncrono da Api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
+import fetchCurrenciesApi from '../services/currenciesApi';
+
 export const SAVE_EMAIL = 'SAVE_EMAIL';
 export const SAVE_CURRENCIES = 'SAVE_CURRENCIES';
+export const SAVE_EXPENSE = 'SAVE_EXPENSE';
 
 export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
@@ -11,10 +14,41 @@ export const saveCurrencies = (currencies) => ({
   payload: currencies,
 });
 
-export const fetchApiThunk = () => (dispatch) => fetch('https://economia.awesomeapi.com.br/json/all')
-  .then((response) => response.json())
-  .then((json) => dispatch(
+export const saveExpenses = (expense) => ({
+  type: SAVE_EXPENSE,
+  payload: expense,
+});
+
+export const fetchCurrenciesThunk = () => async (dispatch) => {
+  const currencieApi = await fetchCurrenciesApi();
+
+  dispatch(
     saveCurrencies(
-      Object.keys(json).filter((currencie) => currencie !== 'USDT'),
+      Object.keys(currencieApi).filter((currencie) => currencie !== 'USDT'),
     ),
-  ));
+  );
+};
+
+export const fetchApiThunk = (expense) => async (dispatch) => {
+  await dispatch(saveExpenses(expense));
+};
+
+// Outras formas de se fazer um thunk creator
+
+// export const fetchCurrenciesThunka = () => (dispatch) => fetch('https://economia.awesomeapi.com.br/json/all')
+//   .then((response) => response.json())
+//   .then((data) => dispatch(
+//     saveCurrencies(
+//       Object.keys(data).filter((currencie) => currencie !== 'USDT'),
+//     ),
+//   ));
+
+// export const fetchCurrenciesThunk = () => async (dispatch) => {
+//   fetch('https://economia.awesomeapi.com.br/json/all')
+//     .then((response) => response.json())
+//     .then((data) => dispatch(
+//       saveCurrencies(
+//         Object.keys(data).filter((currencie) => currencie !== 'USDT'),
+//       ),
+//     ));
+// };
