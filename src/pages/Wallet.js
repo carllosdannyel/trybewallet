00ea9b11840e93227e276a40b8195b4f53ebce6f
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-07-03 16:28:43 -0300

    renomeia o thunk creator e calcula o valor da moeda

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,31 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import { fetchApiThunk } from '../actions';
+import { fetchCurrenciesThunk } from '../actions';
 import FormExpense from '../components/FormExpense';
 
 class Wallet extends React.Component {
-  componentDidMount = async () => {
-    const { fetchApi } = this.props;
-    await fetchApi();
+  state = {
+    total: 0,
+  }
+
+  componentDidMount = () => {
+    const { fetchCurrencies } = this.props;
+    fetchCurrencies();
+  };
+
+  getTotal = () => {
+    const { expenses } = this.props;
+    const total = expenses.reduce((acc, { value, exchangeRates, currency }) => {
+      const soma = acc + (value * exchangeRates[currency].ask);
+      return soma;
+    }, 0);
+    this.setState({ total });
   };
 
   render() {
+    const { total } = this.state;
     return (
       <div>
-        <Header />
-        <FormExpense />
+        <Header total={ total } />
+        <FormExpense saveTotal={ this.getTotal } />
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchApi: () => dispatch(fetchApiThunk()),
-});
-
 const mapStateToProps = (state) => ({
   user: state.user,
+  expenses: state.wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrencies: () => dispatch(fetchCurrenciesThunk()),
 });
 
 Wallet.propTypes = {
